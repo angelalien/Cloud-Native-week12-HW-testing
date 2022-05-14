9b8bf644c681d07ff8341389acc550329dad450e
Angela <angelalien1221@gmail.com>
2022-05-14 21:24:35 +0800

    second case(buy 2 books)

--- apps/kata/src/app/shopping.ts
+++ apps/kata/src/app/shopping.ts
@@ -1,7 +1,16 @@
 export class Shopping {
-    private _price = 0;
-    buy(book: number) {
-        this._price += book*100;
+    private _price=0;
+    private discount=[1,0.95,0.9,0.8,0.75];
+    buy(books: number[]) {
+        let __price = 0;
+        let num=0
+        books.forEach(function (book) {
+            __price += book*100;
+            if(book>0){
+                num=num+1; 
+            }                       
+        });               
+        this._price=__price*this.discount[num-1];
     }
 
     get price() {
