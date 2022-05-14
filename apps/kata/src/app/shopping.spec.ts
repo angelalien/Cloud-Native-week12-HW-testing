7058496a7ec6e9e8552897f922aa3cdb82f2a518
Angela <angelalien1221@gmail.com>
2022-05-14 21:22:57 +0800

    second case(buy 2 books)

--- apps/kata/src/app/shopping.spec.ts
+++ apps/kata/src/app/shopping.spec.ts
@@ -7,7 +7,13 @@ describe('Shopping', () => {
 
   test('buy 1 book(ep1)', () => {
     const shopping = new Shopping();
-    shopping.buy(1);    
+    shopping.buy([1,0,0,0,0]);    
     expect(shopping.price).toBe(100);
+  });
+
+  test('buy 2 books(ep1,2)', () => {
+    const shopping = new Shopping();
+    shopping.buy([1,1,0,0,0]);    
+    expect(shopping.price).toBe(190);
   });
 });
