e04b1cac8228f4cecaf6818c6534968c2c47dbe9
Angela <angelalien1221@gmail.com>
2022-05-14 22:42:08 +0800

    fifth case(buy 8 books)

--- apps/kata/src/app/shopping.ts
+++ apps/kata/src/app/shopping.ts
@@ -16,6 +16,15 @@ export class Shopping {
         for (let i = 0; i < nums.length; i++) {
             this._price+=100*nums[i]*this.discount[nums[i]-1]
         } 
+        if(nums.length>1 && nums[0]>nums[1] && (nums[0]-nums[1])%2==0){
+            let temp_price=0;
+            nums[0]-=1;
+            nums[1]+=1;
+            for (let i = 0; i < nums.length; i++) {
+                temp_price+=100*nums[i]*this.discount[nums[i]-1]
+            }
+            this._price=Math.min(this._price,temp_price);
+        }
     }
 
     get price() {
