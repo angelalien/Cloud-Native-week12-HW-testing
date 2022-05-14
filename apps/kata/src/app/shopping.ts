4a3d9aac0aa371c8aeff12db131f3b1643a7f2d7
Angela <angelalien1221@gmail.com>
2022-05-14 22:11:30 +0800

    fourth case(buy 4 books)

--- apps/kata/src/app/shopping.ts
+++ apps/kata/src/app/shopping.ts
@@ -2,16 +2,20 @@ export class Shopping {
     private _price=0;
     private discount=[1,0.95,0.9,0.8,0.75];
     buy(books: number[]) {
-        let num=0
-        let no_discount_num=0
+        let nums=[0];        
         books.forEach(function (book) {
-            
-            if(book>0){
-                num=num+1; 
-                no_discount_num += book-1;
-            }                       
-        });               
-        this._price=100*num*this.discount[num-1]+100*no_discount_num;
+            for (let i = 0; i < book; i++) {
+                if(nums[i]==null){
+                    nums.push(1);
+                }
+                else{
+                    nums[i]+=1;
+                }
+            }                      
+        });  
+        for (let i = 0; i < nums.length; i++) {
+            this._price+=100*nums[i]*this.discount[nums[i]-1]
+        } 
     }
 
     get price() {
